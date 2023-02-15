92a4a0040e0f68a0d4a93ee078524241cbf4755d
japneet250 <123526380+japneet250@users.noreply.github.com>
2023-02-15 17:33:31 -0500

    Add files via upload

--- L04/lab4.sh
+++ L04/lab4.sh
@@ -3,7 +3,7 @@
 # Use First Argument As Working Directory
 WDIR="$1"
 cd "$WDIR"
-
+filename="file1.txt"
 if [ "$2" -eq 1 ] ; then
     if [ -f file1.txt ] ; then
          true
@@ -31,39 +31,17 @@ elif [ "$2" -eq 3 ] ; then
     fi       
 
 
+fi 
 
-elif [ $# == 3 ] ; then
-    if [ "$2" -eq 1 ] ; then
-      if [ -f $3 ] ; then
-         true
-      else 
-        touch $3
-        echo "singj93" > $3
+if [ $# -eq 3 ] ; then
+  filename="$3"
+else: 
 
-      fi        
-    elif [ "$2" -eq 2 ] ; then
-       if [ -f $3 ] ; then
-          if [ -d backup ] ; then
-             cp "$3" ./backup
-          else 
-             mkdir backup
-             cp "$3" ./backup
-          fi   
-       fi
+   filename
 
-    elif [ "$2" -eq 3 ] ; then   
-       if [ -f backup/$3 ] ; then
-           echo backup/$3 > ./
-           rm backup/$3
-        else
-          true
-        fi    
-    fi
+fi 
 
-    
-
-
-elif [ $# -gt 3 ] ; then
+if [ $# -gt 3 ] ; then
    if [ -f excess.txt ] ; then
      echo excess.txt > ./
     else 
