34cec8bdb8e675cdd0438ae1898d8f87fe06e2b9
japneet250 <123526380+japneet250@users.noreply.github.com>
2023-03-30 21:46:55 -0400

    Update student.h

--- L09/src/student.h
+++ L09/src/student.h
@@ -1,4 +1,13 @@
- 
+ /**
+
+@brief A struct representing a student with their first name, last name, ID number, grades, and the number of grades.
+
+
+The Student struct is used to hold information about a student, including their first name, last name, ID number,
+a pointer to their grades, and the number of grades they have. The grades are stored as a dynamic array of double values.
+Functions are provided to add a grade to the student's record, calculate the average grade, print the student's information,
+and generate a random student with a specified number of grades.
+*/
 typedef struct _student 
 { 
   char first_name[50];
